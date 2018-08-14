bc7b64edd4dded54bd351471725a3d8277eea157
ac-alpha <ashutoshchaubey2999@gmail.com>
2018-08-15 03:01:10 +0530

    Documented the Constants.java class

--- app/src/main/java/in/ashutoshchaubey/forgotnot/constants/Constants.java
+++ app/src/main/java/in/ashutoshchaubey/forgotnot/constants/Constants.java
@@ -8,26 +8,39 @@ import android.provider.BaseColumns;
 
 public class Constants {
 
+    /**
+     * Defines the setOrNot property of the NotifItem
+     * REMINDER_SET - User has set the reminder and Event is added
+     * REMINDER_UNSET - User has decided not to set reminder for the Event
+     * REMINDER_UNNOTICED - User has not seen the Event till and not decided
+     *                      whether to add reminder or not
+     */
     public static String REMINDER_SET = "Reminder Set";
     public static String REMINDER_UNSET = "Reminder Not Set";
     public static String REMINDER_UNNOTICED = "Unnoticed";
+
+    /**
+     * Defines the showOrNot property of the NotifItem
+     * NOTIF_SHOW - Add the NotifItem to the RecyclerView
+     * NOTIF_HIDE - Don't dispay the NotifItem in the RecyclerView
+     */
     public static int NOTIF_SHOW = 1;
     public static int NOTIF_HIDE = 0;
-    public static String KEY_TOTAL_NO_OF_DELETED_NOTIFITEMS = "total_no_of_deleted_notif_items1";
+
 
     public class NotifEntry implements BaseColumns{
 
-        public static final String TABLE_NAME = "notifs";
-        public static final String _ID = BaseColumns._ID;
-        public static final String COLUMN_EVENT_NAME = "name";
-        public static final String COLUMN_DATE_DD = "date_dd";
-        public static final String COLUMN_DATE_MM = "date_mm";
-        public static final String COLUMN_DATE_YYYY = "date_yyyy";
-        public static final String COLUMN_TIME_HR = "time_hr";
-        public static final String COLUMN_TIME_MIN = "time_min";
+        public static final String TABLE_NAME = "notifs"; //Table Name for the notificaion list table
+        public static final String _ID = BaseColumns._ID; //Default _ID
+        public static final String COLUMN_EVENT_NAME = "name";  //Title of the event
+        public static final String COLUMN_DATE_DD = "date_dd";  //DD of the Event in DDMMYYYY format
+        public static final String COLUMN_DATE_MM = "date_mm";  //MM of the Event in DDMMYYYY format
+        public static final String COLUMN_DATE_YYYY = "date_yyyy";  //YYYY of the Event in DDMMYYYY format
+        public static final String COLUMN_TIME_HR = "time_hr";  //Hour of the event in 12 Hr format
+        public static final String COLUMN_TIME_MIN = "time_min";  //Minute of the event
         public static final String COLUMN_TIME_AM_PM = "time_am_pm";
-        public static final String COLUMN_SET_UNSET = "set_unset";
-        public static final String COLUMN_SHOW = "show_notif_in_list";
+        public static final String COLUMN_SET_UNSET = "set_unset";  //The event's reminder is set, unset or unnoticed
+        public static final String COLUMN_SHOW = "show_notif_in_list";  //The event should be shown in the list or not
 
     }
 
