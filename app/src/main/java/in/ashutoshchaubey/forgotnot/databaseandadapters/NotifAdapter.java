507e5880f9efc4465aff93a54e9beddfe6a27237
ac-alpha <ashutoshchaubey2999@gmail.com>
2018-08-17 00:35:32 +0530

    Documented NotifAdapter

--- app/src/main/java/in/ashutoshchaubey/forgotnot/databaseandadapters/NotifAdapter.java
+++ app/src/main/java/in/ashutoshchaubey/forgotnot/databaseandadapters/NotifAdapter.java
@@ -20,25 +20,46 @@ import in.ashutoshchaubey.forgotnot.constants.Constants;
 
 public class NotifAdapter extends RecyclerView.Adapter<NotifAdapter.NotifViewHolder> {
 
+    /**
+     * ItemClickListener to listen onClick events
+     */
     private ItemClickListener mClickListener;
+
+    /**
+     * LayoutInflater to inflate ViewHolders to the RecyclerView
+     */
     private LayoutInflater mInflater;
+
+    /**
+     * ArrayList to store the data in form of NotifItems to populate to the RecyclerView
+     */
     private ArrayList<NotifItem> mData;
 
     public NotifAdapter(Context context, ArrayList<NotifItem> data) {
+        //Initializing the variables
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
     }
 
     @Override
     public NotifAdapter.NotifViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        //Inflating one ViewHolder to the RecyclerView
         View view = mInflater.inflate(R.layout.notif_item, parent, false);
+
         return new NotifViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(NotifAdapter.NotifViewHolder holder, int position) {
+
+        //setting notifTitle's text as the title FIELD of the NotifItem
         holder.notifTitle.setText(mData.get(position).getTitle());
+
+        //setting dateTime's text to the date in specific format
         holder.dateTIme.setText(String.format("%1$tA %1$tb %1$td %1$tY at %1$tI:%1$tM %1$Tp", mData.get(position).getReminderTime()));
+
+        //setting the text and background of setOrNot as per the setOrNot field of NotifItem
         if (mData.get(position).getSetOrNot().equals(Constants.REMINDER_SET)) {
             holder.setOrNot.setBackgroundResource(R.drawable.green_button_bg);
             holder.setOrNot.setText(Constants.REMINDER_SET);
@@ -49,6 +70,8 @@ public class NotifAdapter extends RecyclerView.Adapter<NotifAdapter.NotifViewHol
             holder.setOrNot.setBackgroundResource(R.drawable.red_button_bg);
             holder.setOrNot.setText(Constants.REMINDER_UNNOTICED);
         }
+
+        //If reminder is not set then making set button visible
         if (mData.get(position).getSetOrNot().equals(Constants.REMINDER_SET)) {
             holder.setReminder.setVisibility(View.INVISIBLE);
         } else {
@@ -64,21 +87,37 @@ public class NotifAdapter extends RecyclerView.Adapter<NotifAdapter.NotifViewHol
     }
 
     public class NotifViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        //Title of the notification
         TextView notifTitle;
+
+        //TextView to display Date and Time of reminder
         TextView dateTIme;
+
+        //Clickable TextView to set reminder if it is not already set
         TextView setReminder;
+
+        //TextView to indicate whether reminder is set or not
         TextView setOrNot;
-        public RelativeLayout viewBackground;
+
+        //Background of the view which appears when swiped
+        RelativeLayout viewBackground;
+
+        //Foreground View, i/e/ basically the notif_item's front view
         public LinearLayout viewForeground;
 
-        public NotifViewHolder(View itemView) {
+        NotifViewHolder(View itemView) {
             super(itemView);
+
+            //Fetching the Views from xml
             notifTitle = (TextView) itemView.findViewById(R.id.notif_title);
             dateTIme = (TextView) itemView.findViewById(R.id.date_time);
             setReminder = (TextView) itemView.findViewById(R.id.set_reminder);
             setOrNot = (TextView) itemView.findViewById(R.id.set_or_not);
             viewBackground = (RelativeLayout)itemView.findViewById(R.id.background_view);
             viewForeground = (LinearLayout) itemView.findViewById(R.id.foreground_view);
+
+            //setting onClickListener to be current class
             setReminder.setOnClickListener(this);
         }
 
@@ -108,11 +147,20 @@ public class NotifAdapter extends RecyclerView.Adapter<NotifAdapter.NotifViewHol
         this.mData = mData;
     }
 
+    /**
+     * Function to remove a NotifItem from the RecyclerView
+     * @param pos - Position of the NotifItem to be deleted
+     */
     public void removeItem(int pos){
         mData.remove(pos);
         notifyItemRemoved(pos);
     }
 
+    /**
+     * Function to restore a previously deleted item
+     * @param pos - Position at which the NotifItem is to be added
+     * @param item - NotifItem to be added
+     */
     public void restoreItem(int pos, NotifItem item){
         mData.add(pos, item);
         notifyItemInserted(pos);
